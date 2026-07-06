21ecfaec4a5223fda3a5ac2cba8fe868c75148d1
TymofiiKotliar <xkotliar@stuba.sk>
2026-07-07 00:45:48 +0200

    style: textbox in profile editor

--- src/MediaFlow.Presentation/Views/ProfileEditorView.axaml.cs
+++ src/MediaFlow.Presentation/Views/ProfileEditorView.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
 using MediaFlow.Presentation.ViewModels;
 
@@ -13,6 +14,30 @@ public partial class ProfileEditorView : UserControl
     {
         InitializeComponent();
         DataContextChanged += OnDataContextChanged;
+
+        // TextBox's watermark is only hidden while Text is non-empty; there's no built-in
+        // way to also hide it while the box has focus but is still empty, so we toggle
+        // Watermark itself and stash the original text on the Tag property.
+        AddHandler(InputElement.GotFocusEvent, OnTextBoxGotFocus, RoutingStrategies.Bubble);
+        AddHandler(InputElement.LostFocusEvent, OnTextBoxLostFocus, RoutingStrategies.Bubble);
+    }
+
+    private void OnTextBoxGotFocus(object? sender, GotFocusEventArgs e)
+    {
+        if (e.Source is TextBox textBox && !string.IsNullOrEmpty(textBox.Watermark))
+        {
+            textBox.Tag = textBox.Watermark;
+            textBox.Watermark = null;
+        }
+    }
+
+    private void OnTextBoxLostFocus(object? sender, RoutedEventArgs e)
+    {
+        if (e.Source is TextBox textBox && textBox.Tag is string watermark)
+        {
+            textBox.Watermark = watermark;
+            textBox.Tag = null;
+        }
     }
 
     private void OnDataContextChanged(object? sender, EventArgs e)
